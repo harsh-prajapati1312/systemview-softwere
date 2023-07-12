1d47283d54b294cc757821542cbb508b5b7196e5
harsh-prajapati1312 <113959353+harsh-prajapati1312@users.noreply.github.com>
2023-07-12 16:51:12 +0530

    updated files 12-07-2023

--- check2.py
+++ check2.py
@@ -3,12 +3,14 @@ import win32process
 import win32gui
 from datetime import datetime
 import threading
+import time
 time_interval=5
 def get_process_start_time(pid):
     process = psutil.Process(pid)
     start_time = process.create_time()
     return datetime.fromtimestamp(start_time).strftime("%H:%M:%S")
-
+global l
+l=[]
 def get_process_info():
     timer = threading.Timer(time_interval,get_process_info)
     timer.start()
@@ -18,10 +20,13 @@ def get_process_info():
     start_time = get_process_start_time(pid)
     current_time = datetime.now().strftime("%H:%M:%S")
     time_diff = datetime.strptime(current_time, "%H:%M:%S") - datetime.strptime(start_time, "%H:%M:%S")
+    today=time.strftime("%d/%m/%Y")
     hours = time_diff.seconds // 3600
     minutes = (time_diff.seconds // 60) % 60
     seconds = time_diff.seconds % 60
-    lst=[pid, process_name, hours, ":", minutes, ":", seconds]
+    lst=[today,current_time,pid, process_name, hours, minutes, seconds]
+    global l
+    l.append(lst)
     print(lst)
 
 get_process_info()
